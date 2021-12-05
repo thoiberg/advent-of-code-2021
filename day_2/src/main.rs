36465397e82b4d7448097d63af022daddbaf2b40
Tim Hoiberg <tim.hoiberg@gmail.com>
2021-12-05 17:17:31 +1000

    Add Day 2: Part Two solution

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -7,8 +7,11 @@ fn main() {
 
     let data = read_and_process_input();
 
-    let part_one_answer = part_one_solution(&data.unwrap());
-    println!("Part One Solution is: {}", part_one_answer);
+    // let part_one_answer = part_one_solution(&data.unwrap());
+    // println!("Part One Solution is: {}", part_one_answer);
+
+    let part_two_answer = part_two_solution(&data.unwrap());
+    println!("Part Two Solution is: {}", part_two_answer)
 }
 
 #[derive(Debug, PartialEq, EnumString, Clone)]
@@ -32,22 +35,20 @@ struct Movement {
 struct Coordinate {
     horizontal: i32,
     depth: i32,
+    aim: i32,
 }
 
 fn part_one_solution(data: &Vec<Movement>) -> i32 {
     let start_point = Coordinate {
         horizontal: 0,
         depth: 0,
+        aim: 0,
     };
 
     let final_coordinate = pilot_submarine(start_point, data);
 
     final_coordinate.horizontal * final_coordinate.depth
 }
-
-// forward X increases the horizontal position by X units.
-// down X increases the depth by X units.
-// up X decreases the depth by X units.
 
 fn pilot_submarine(coordinate: Coordinate, movements: &Vec<Movement>) -> Coordinate {
     let movement = movements.first();
@@ -63,6 +64,39 @@ fn pilot_submarine(coordinate: Coordinate, movements: &Vec<Movement>) -> Coordin
             }
 
             return pilot_submarine(new_coordinate, &movements[1..].to_vec());
+        }
+        None => return coordinate,
+    }
+}
+
+fn part_two_solution(data: &Vec<Movement>) -> i32 {
+    let start_point = Coordinate {
+        horizontal: 0,
+        depth: 0,
+        aim: 0,
+    };
+
+    let final_coordinate = pilot_and_aim(start_point, data);
+    final_coordinate.horizontal * final_coordinate.depth
+}
+
+fn pilot_and_aim(coordinate: Coordinate, movements: &Vec<Movement>) -> Coordinate {
+    let movement = movements.first();
+
+    match movement {
+        Some(x) => {
+            let mut new_coordinate = coordinate;
+
+            match x.direction {
+                Direction::Forward => {
+                    new_coordinate.horizontal += x.distance;
+                    new_coordinate.depth += new_coordinate.aim * x.distance;
+                }
+                Direction::Down => new_coordinate.aim += x.distance,
+                Direction::Up => new_coordinate.aim -= x.distance,
+            }
+
+            return pilot_and_aim(new_coordinate, &movements[1..].to_vec());
         }
         None => return coordinate,
     }
@@ -86,9 +120,8 @@ fn read_and_process_input() -> Result<Vec<Movement>, ioError> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part_one_example() {
-        let data = vec![
+    fn test_data() -> Vec<Movement> {
+        vec![
             Movement {
                 direction: Direction::from_str("forward").unwrap(),
                 distance: 5,
@@ -113,9 +146,12 @@ mod tests {
                 direction: Direction::from_str("forward").unwrap(),
                 distance: 2,
             },
-        ];
+        ]
+    }
 
-        let part_one_answer = part_one_solution(&data);
+    #[test]
+    fn test_part_one_example() {
+        let part_one_answer = part_one_solution(&test_data());
 
         assert_eq!(part_one_answer, 150);
     }
@@ -124,5 +160,16 @@ mod tests {
     fn test_part_one_solution() {
         let data = read_and_process_input().unwrap();
         assert_eq!(part_one_solution(&data), 1670340);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        assert_eq!(part_two_solution(&test_data()), 900);
+    }
+
+    #[test]
+    fn test_part_two_solution() {
+        let data = read_and_process_input().unwrap();
+        assert_eq!(part_two_solution(&data), 1954293920);
     }
 }
