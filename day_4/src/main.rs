f447dcc6476dd6d739bafdbf3521e29eafbd0451
Tim Hoiberg <tim.hoiberg@gmail.com>
2021-12-11 23:58:46 +1000

    Use recursion instead of loops
    This seems to avoid some of the annoyances I was having trying to get
    mutable references to work.

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -5,39 +5,37 @@ use bingo_board::*;
 fn main() {
     let data = read_and_process_input();
 
-    let part_one_answer = part_one_solution(data.0, &data.1);
+    let part_one_answer = part_one_solution(&data.0, &data.1);
 
     println!("Part One Solution is: {}", part_one_answer);
 
-    let part_two_answer = part_two_solution(data.0, &data.1);
+    let part_two_answer = part_two_solution(&data.0, &data.1);
 
     println!("Part Two Solution is: {}", part_two_answer);
 }
 
-fn part_one_solution(bingo_calls: &'static str, boards: &Vec<BingoBoard>) -> i32 {
-    let calls: Vec<&str> = bingo_calls.trim().split(",").collect();
-    let mut answer = 0;
-    let mut mut_boards = boards.to_owned();
+fn part_one_solution(bingo_calls: &Vec<i32>, boards: &Vec<BingoBoard>) -> i32 {
+    fn process_call(bingo_calls: &Vec<i32>, boards: &Vec<BingoBoard>) -> (BingoBoard, i32) {
+        let call = bingo_calls.first().unwrap().to_owned();
+        let mut mut_boards = boards.to_owned();
 
-    for call in calls {
-        let call_int = call.parse::<i32>().unwrap();
         for board in &mut mut_boards {
-            let winner = board.mark(call_int);
+            let winner = board.mark(call);
 
             if winner.to_owned() {
-                answer = board.unused_spaces() * call_int
+                return (board.to_owned(), call);
             }
         }
 
-        if answer > 0 {
-            return answer;
-        }
+        return process_call(&bingo_calls[1..].to_vec(), &mut_boards);
     }
 
-    answer
+    let (winning_board, last_call) = process_call(bingo_calls, boards);
+
+    winning_board.unused_spaces() * last_call
 }
 
-fn part_two_solution(bingo_calls: &'static str, boards: &Vec<BingoBoard>) -> i32 {
+fn part_two_solution(bingo_calls: &Vec<i32>, boards: &Vec<BingoBoard>) -> i32 {
     // same as part one, but when a board wins, we take it out of the running
     // the last board to win is the winner
     // if i just look for the last winning board when I finish I'll find all the boards that have
@@ -46,7 +44,7 @@ fn part_two_solution(bingo_calls: &'static str, boards: &Vec<BingoBoard>) -> i32
     0
 }
 
-fn read_and_process_input() -> (&'static str, Vec<BingoBoard>) {
+fn read_and_process_input() -> (Vec<i32>, Vec<BingoBoard>) {
     process_input(read())
 }
 
@@ -54,24 +52,30 @@ fn read() -> &'static str {
     include_str!("../data/puzzle_data")
 }
 
-fn process_input(data: &'static str) -> (&'static str, Vec<BingoBoard>) {
+fn process_input(data: &'static str) -> (Vec<i32>, Vec<BingoBoard>) {
     let mut input_data: Vec<&str> = data.split("\n\n").collect();
 
-    let bingo_call = input_data.remove(0);
+    let call_string = input_data.remove(0);
+    let calls: Vec<i32> = call_string
+        .trim()
+        .split(",")
+        .into_iter()
+        .map(|call| call.parse::<i32>().unwrap())
+        .collect();
 
     let boards: Vec<BingoBoard> = input_data
         .into_iter()
         .map(|board_layout| BingoBoard::new(board_layout))
         .collect();
 
-    (bingo_call, boards)
+    (calls, boards)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn test_data() -> (&'static str, Vec<BingoBoard>) {
+    fn test_data() -> (Vec<i32>, Vec<BingoBoard>) {
         let data = include_str!("../data/test_data");
 
         process_input(data)
@@ -80,13 +84,20 @@ mod tests {
     #[test]
     fn test_part_one_example() {
         let data = test_data();
-        assert_eq!(part_one_solution(data.0, &data.1), 4512);
+        assert_eq!(part_one_solution(&data.0, &data.1), 4512);
     }
 
     #[test]
     fn test_part_one_solution() {
         let data = read_and_process_input();
 
-        assert_eq!(part_one_solution(data.0, &data.1), 44736);
+        assert_eq!(part_one_solution(&data.0, &data.1), 44736);
+    }
+
+    #[test]
+    fn test_part_two_example() {
+        let data = test_data();
+
+        assert_eq!(part_two_solution(&data.0, &data.1), 1924);
     }
 }
