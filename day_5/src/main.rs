6f771290f1cb520b098a51b6d87298f7024e1512
Tim Hoiberg <tim.hoiberg@gmail.com>
2021-12-17 17:56:43 +1000

    Initial setup for day 5

---
+++ day_5/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// process data
+// discard any that aren't horizontal or vertical
+// loop through the rest and create hash of points (combine the x+y into a single variable) (or check out nd_array????)
+//   then increment the variable
+// loop through all final fields and count the fields that have 2 or more lines
